df50e877de57235c9e03dfe87d6bcec3b1572333
Sandra-Be <sandraberzk@gmail.com>
2021-09-25 17:31:52 +0000

    Added categories dropdown

--- app.py
+++ app.py
@@ -101,7 +101,8 @@ def logout():
 
 @app.route("/add_spell")
 def add_spell():
-    return render_template("add_spell.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_spell.html", categories=categories)
 
 
 if __name__ == "__main__":
