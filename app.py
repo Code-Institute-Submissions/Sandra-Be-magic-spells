4e2c42b963584327d018d0793f4243e34042e000
Sandra-Be <sandraberzk@gmail.com>
2021-09-25 16:29:13 +0000

    Created add_spell template

--- app.py
+++ app.py
@@ -99,6 +99,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_spell")
+def add_spell():
+    return render_template("add_spell.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
