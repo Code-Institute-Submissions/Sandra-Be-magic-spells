7abc058c69fae8a73ec387639eb1cb0ab060e365
Sandra-Be <sandraberzk@gmail.com>
2021-10-02 20:50:03 +0000

    Added home page

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/home")
+def home():
+    home = mongo.db.home.find()
+    return render_template("home.html", home=home)
+
+
 @app.route("/get_spells")
 def get_spells():
     spells = list(mongo.db.spells.find())
