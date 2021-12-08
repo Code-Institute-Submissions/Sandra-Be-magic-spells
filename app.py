31aa1ae2c2ef6c4e34b466e28fd921ecd2f43329
Sandra-Be <sandraberzk@gmail.com>
2021-12-08 06:44:27 +0000

    Fixed syntax error

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def edit_spell(spell_id):
 def delete_spell(spell_id):
     mongo.db.spells.remove({"_id": ObjectId(spell_id)})
     flash("Spell Deleted")
-    return redirect(url_for("get_spells")
+    return redirect(url_for("get_spells"))
 
 
 # Categories page
