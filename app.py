06d28a620ec85206938415dc9f03ebab3d4c2ec7
Sandra-Be <sandraberzk@gmail.com>
2021-12-09 22:20:41 +0000

    Changed debug to False before re-submission

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ def response_404(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
